4f55bd38c8267d4278f3df9e91cb254db51e913c
Ashley Gao <gaoye0718@gmail.com>
2022-05-08 13:49:10 -0400

    wip

--- core/adapt.py
+++ core/adapt.py
@@ -85,7 +85,7 @@ def train_tgt(src_encoder, tgt_encoder, critic,
             feat_tgt = tgt_encoder(images_tgt)
 
             # predict on discriminator
-            pred_tgt = critic(feat_tgt)
+            pred_tgt = critic(feat_tgt.squeeze_())
 
             # prepare fake labels
             label_tgt = make_variable(torch.ones(feat_tgt.size(0)).long())
